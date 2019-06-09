fb0ee0790efba4e714737d25f55e207531678740
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2019-06-09 12:18:35 +0200

    removed exports and minified parameter

--- Nuttercode-PRTG/Nuttercode-PRTG.psm1
+++ Nuttercode-PRTG/Nuttercode-PRTG.psm1
@@ -241,163 +241,67 @@ Function Convert-PRTGChannelModeToString([PRTGChannelMode]$mode) {
 function Convert-PRTGSensorToXML() {
 	[cmdletbinding()]
 	param(  
-		[Parameter(
-			Position=0, 
-			Mandatory=$true, 
-			ValueFromPipeline=$true)
-		]
-		[PRTGSensor]$sensor
+		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)][PRTGSensor]$sensor
     )
 	Process {
 		return $sensor.toXML()
 	}
 }
-export-modulemember -function Convert-PRTGSensorToXML
 
 
 function Add-PRTGChannel() {
 	[cmdletbinding()]
 	param(
-		[Parameter(
-			Position=0, 
-			Mandatory=$true, 
-			ValueFromPipeline=$true)
-		]
-		[PRTGSensor]$sensor,
-		[Parameter(
-			Position=1, 
-			Mandatory=$true)
-		]
-		[string]$name,
-		[Parameter(
-			Position=2, 
-			Mandatory=$true)
-		]
-		[string]$value,
-		[Parameter(
-			Position=3, 
-			Mandatory=$false)
-		]
-		[PRTGChannelUnit]$unit = [PRTGChannelUnit]::Count,
-		[Parameter(
-			Position=4, 
-			Mandatory=$false)
-		]
-		[PRTGChannelMode]$mode = [PRTGChannelMode]::Absolute,
-		[Parameter(
-			Position=4, 
-			Mandatory=$false)
-		]
-		[PRTGChannelLimit]$limit = $null,
-		[Parameter(
-			Position=5, 
-			Mandatory=$false)
-		]
-		[string]$value_lookup_id = $null,
-		[Parameter(
-			Position=5, 
-			Mandatory=$false)
-		]
-		[switch]$is_float = $false,
-		[Parameter(
-			Position=6, 
-			Mandatory=$false)
-		]
-		[string]$custom_unit = "units"
+		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)][PRTGSensor]$sensor,
+		[Parameter(Position=1, Mandatory=$true)][string]$name,
+		[Parameter(Position=2, Mandatory=$true)][string]$value,
+		[Parameter(Position=3, Mandatory=$false)][PRTGChannelUnit]$unit = [PRTGChannelUnit]::Count,
+		[Parameter(Position=4, Mandatory=$false)][PRTGChannelMode]$mode = [PRTGChannelMode]::Absolute,
+		[Parameter(Position=4, Mandatory=$false)][PRTGChannelLimit]$limit = $null,
+		[Parameter(Position=5, Mandatory=$false)][string]$value_lookup_id = $null,
+		[Parameter(Position=5, Mandatory=$false)][switch]$is_float = $false,
+		[Parameter(Position=6, Mandatory=$false)][string]$custom_unit = "units"
     )
 	Process {
 		$sensor.addChannel($name, $value, $unit, $mode, $limit, $value_lookup_id, $is_float, $custom_unit)
 		return $sensor
 	}
 }
-export-modulemember -function Add-PRTGChannel
 
 
 function New-PRTGSensor() {
 	return [PRTGSensor]::new()
 }
-export-modulemember -function New-PRTGSensor
 
 
 function Set-PRTGSensorText() {
 	[cmdletbinding()]
 	param(  
-		[Parameter(
-			Position=0, 
-			Mandatory=$true, 
-			ValueFromPipeline=$true)
-		]
-		[PRTGSensor]$sensor,
-		[Parameter(
-			Position=1, 
-			Mandatory=$true)
-		]
-		[string]$text
+		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)][PRTGSensor]$sensor,
+		[Parameter(Position=1, Mandatory=$true)][string]$text
     )
 	Process {
 		$sensor.setText($text)
         return $sensor
 	}
 }
-export-modulemember -function Set-PRTGSensorText
 
 
 function New-PRTGChannelLimit() {
 	[cmdletbinding()]
 	param(
-		[Parameter(
-			Position=0, 
-			Mandatory=$false)
-		]
-		[float]$warning_min,
-		[Parameter(
-			Position=1, 
-			Mandatory=$false)
-		]
-		[float]$warning_max,
-		[Parameter(
-			Position=2, 
-			Mandatory=$false)
-		]
-		[float]$error_min,
-		[Parameter(
-			Position=3, 
-			Mandatory=$false)
-		]
-		[float]$error_max,
-		[Parameter(
-			Position=4, 
-			Mandatory=$false)
-		]
-		[switch]$DisableMin,
-		[Parameter(
-			Position=5, 
-			Mandatory=$false)
-		]
-		[switch]$DisableMax,
-		[Parameter(
-			Position=6, 
-			Mandatory=$false)
-		]
-		[switch]$DisableWarningMin,
-		[Parameter(
-			Position=7, 
-			Mandatory=$false)
-		]
-		[switch]$DisableWarningMax,
-		[Parameter(
-			Position=8, 
-			Mandatory=$false)
-		]
-		[switch]$DisableErrorMin,
-		[Parameter(
-			Position=9, 
-			Mandatory=$false)
-		]
-		[switch]$DisableErrorMax
+		[Parameter(Position=0, Mandatory=$false)][float]$warning_min,
+		[Parameter(Position=1, Mandatory=$false)][float]$warning_max,
+		[Parameter(Position=2, Mandatory=$false)][float]$error_min,
+		[Parameter(Position=3, Mandatory=$false)][float]$error_max,
+		[Parameter(Position=4, Mandatory=$false)][switch]$DisableMin,
+		[Parameter(Position=5, Mandatory=$false)][switch]$DisableMax,
+		[Parameter(Position=6, Mandatory=$false)][switch]$DisableWarningMin,
+		[Parameter(Position=7, Mandatory=$false)][switch]$DisableWarningMax,
+		[Parameter(Position=8, Mandatory=$false)][switch]$DisableErrorMin,
+		[Parameter(Position=9, Mandatory=$false)][switch]$DisableErrorMax
     )
 	Process {
 		return [PRTGChannelLimit]::new($warning_min, $warning_max, $error_min, $error_max, $DisableMin, $DisableMax, $DisableWarningMin, $DisableWarningMax, $DisableErrorMin, $DisableErrorMax)
 	}
 }
-export-modulemember -function New-PRTGChannelLimit
