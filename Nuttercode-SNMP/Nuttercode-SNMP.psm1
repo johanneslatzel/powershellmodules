cd1a59d53ee667beffa25a252f997c2548907a75
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2019-09-09 21:23:04 +0200

    changed code to SnmpSharpNet
    uses SnmpSharpNet instead of Com-Objects

--- Nuttercode-SNMP/Nuttercode-SNMP.psm1
+++ Nuttercode-SNMP/Nuttercode-SNMP.psm1
@@ -2,15 +2,13 @@
 #
 # Author: Johannes B. Latzel
 # 
-# Version: 2019.06.09
+# Version: 2019.09.09
 #
 ########################################################################################################################
 #
 # Description:
 #
-#     applies the methods of the olePrn.OleSNMP Com-Object and wraps them into 4 easy to use CMDlets:
-#         Get-Snmp, Get-SnmpValue, Get-SnmpWalk, Get-SnmpWalkValue
-#     (uses: https://docs.microsoft.com/en-us/windows-hardware/drivers/print/isnmp-methods)
+#     uses "http://www.snmpsharpnet.com/" SnmpSharpNet to provide easy-to-use Snmp Powershell-CMDlets
 #
 ########################################################################################################################
 #
@@ -20,26 +18,25 @@
 #
 ########################################################################################################################
 
+Add-Type -Path "SnmpSharpNet.dll"
+
 class SNMPResult {
 	[string]$Oid
     [string]$Value
-    SNMPResult([string]$Oid,[string]$Value) {
+    SNMPResult([string]$Oid, [string]$Value) {
         $this.Oid = $Oid
         $this.Value = $Value
     }
 }
 
-function Init-Snmp([string]$Hostname, [string]$Community = "public") {
-    $comSnmp = New-Object -ComObject olePrn.OleSNMP
-    $comSnmp.Open($Hostname, $Community)
-    return $comSnmp
-}
-
-function Prepare-Oid([string]$Oid) {
-    if( -not $Oid.StartsWith(".") ) {
-        $Oid = ".$Oid"
+function Get-IpAddress([string]$Hostname) {
+    [System.Net.IPHostEntry]$hostEntry = [System.Net.Dns]::GetHostEntry($Hostname)
+    if( $hostEntry.AddressList.Length -gt 0 ) {
+        return $hostEntry.AddressList[0]
     }
-    return $Oid.Trim()
+    else {
+        throw [Exception]::new("$Hostname could not be resolved")
+    }
 }
 
 function Get-SnmpWalk() {
@@ -48,17 +45,39 @@ function Get-SnmpWalk() {
 		[Parameter(Position=1, Mandatory=$true)][string]$Oid,
 		[Parameter(Position=2, Mandatory=$false)][string]$Community = "public"
 	)
-    try { 
-	    $comSnmp = Init-Snmp -Hostname $Hostname -Community $Community
-        $result = $comSnmp.GetTree((Prepare-Oid -Oid $Oid))
-        $comSnmp.Close()
+    $resultArray = @()
+    $udpTarget = [SnmpSharpNet.UdpTarget]::new((Get-IpAddress -Hostname $Hostname), 161, 5000, 1)
+    $pdu = [SnmpSharpNet.Pdu]::new([SnmpSharpNet.PduType]::GetBulk)
+    $agentParameters = [SnmpSharpNet.AgentParameters]::new([SnmpSharpNet.OctetString]::new($Community))
+    $agentParameters.Version = [SnmpSharpNet.SnmpVersion]::Ver2
+    $rootOid = [SnmpSharpNet.Oid]::new($Oid)
+    $lastOid = [SnmpSharpNet.Oid]::new($Oid)
+    $resultArray = @()
+    while( $lastOid -ne $null ) {
+        if( $pdu.RequestId -ne 0 ) {
+            $pdu.RequestId++
+        }
+        $pdu.VbList.Clear()
+        $pdu.VbList.Add($lastOid)
+        $result = [SnmpSharpNet.SnmpV2Packet] $udpTarget.Request($pdu, $agentParameters)
+        $lastOid = $null
+        if( $result -ne $null ) {
+            foreach( $v in $result.Pdu.VbList ) {
+                if( $rootOid.IsRootOf($v.Oid) ) {
+                    if( $v.Value.Type -ne [SnmpSharpNet.SnmpConstants]::SMI_ENDOFMIBVIEW ) {
+                        try {
+                            $resultArray += [SNMPResult]::new([string]::Join(".", $v.Oid.ToString().Split(" ")), $v.Value.ToString())
+                        }
+                        catch {}
+                        $lastOid = $v.Oid
+                    }
+                }
+            }
+
+        }
     }
-    catch {
-        $result = @()
-    }
-    for($a = 0; $a -lt $result.Count / 2; $a++) {
-        [SNMPResult]::new(".$($comSnmp.OIDFromString($($result[0, $a])) -join ".")", "$($result[1, $a])")
-    }
+    $udpTarget.Close()
+    $resultArray
 }
 
 
@@ -68,14 +87,7 @@ function Get-SnmpWalkValue() {
 		[Parameter(Position=1, Mandatory=$true)][string]$Oid,
 		[Parameter(Position=2, Mandatory=$false)][string]$Community = "public"
 	)
-	Get-SNMPWalk -Hostname $Hostname -Oid $Oid -Community $Community | % {
-        if( $_ -ne $null ) {
-            $_.Value
-        }
-	    else {
-            ""
-        }
-    }
+	Get-SNMPWalk -Hostname $Hostname -Oid $Oid -Community $Community | % {$_.Value}
 }
 
 
@@ -86,11 +98,16 @@ function Get-Snmp() {
 		[Parameter(Position=2, Mandatory=$false)][string]$Community = "public"
 	)
     try {
-        $comSnmp = Init-Snmp -Hostname $Hostname -Community $Community
-        $result = $comSnmp.Get((Prepare-Oid -Oid $Oid))
-        $comSnmp.Close()
+        $udpTarget = [SnmpSharpNet.UdpTarget]::new((Get-IpAddress -Hostname $Hostname), 161, 5000, 1)
+        $pdu = [SnmpSharpNet.Pdu]::new([SnmpSharpNet.PduType]::Get)
+        $pdu.VbList.Add($Oid)
+        $agentParameters = [SnmpSharpNet.AgentParameters]::new([SnmpSharpNet.OctetString]::new($Community))
+        $agentParameters.Version = [SnmpSharpNet.SnmpVersion]::Ver2
+        $result = ([SnmpSharpNet.SnmpV2Packet] $udpTarget.Request($pdu, $agentParameters)).Pdu.VbList[0].Value.ToString()
+        $udpTarget.Close()
     }
     catch {
+        Write-Host $_
         $result = ""
     }
     [SNMPResult]::new($Oid, $result)
